2b4068b50e3919bc6c669649f3be204cc264d755
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-10-04 13:55:07 -0400

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,15 @@ public class Main {
      CrewMember crewMember2 = new CrewMember("Jeffery", FlightCrewJob.CO_PILOT, 90000.00f);
      CrewMember crewMember3 = new CrewMember("Carol",FlightCrewJob.FLIGHT_ATTENDANT, 70000.0f);
 
-     Ticket ticket = new Ticket("");
-     BusTicket busTicket = new BusTicket(10,"","","",0);
+
+     BusTicket busTicket = new BusTicket(10,"Wilmington","Newark","057B",50.0);
+     PlaneTicket planeTicket = new PlaneTicket(2,"Brooklyn","Germany", "007A", 100.0);
 
 
     Passenger passenger57 = new Passenger("Joe",1000.00);
     Passenger passenger07 = new Passenger("Larry",800.00);
 
+        System.out.println(passenger57.printTicket(planeTicket)););
 
 
 
--- src/main/java/PlaneTicket.java
+++ src/main/java/PlaneTicket.java
@@ -2,6 +2,10 @@ public class PlaneTicket extends Ticket{
 
     private final double price = 100.00;
 
+    public PlaneTicket(int id, String origin, String destination, String seatNumber, double price) {
+        super(id, origin, destination, seatNumber, price);
+    }
+
     public boolean isAFrequentFlyer(){
         return true;
     }
